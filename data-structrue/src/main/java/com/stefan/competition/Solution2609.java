adf6a5c2f8ab269f2418011baf510876dd69119c
StefanYang <923775370@qq.com>
2023-04-04 15:46:22 +0800

    20230404-practice-双指针

---
+++ data-structrue/src/main/java/com/stefan/competition/Solution2609.java
@@ -0,0 +1,27 @@
+package com.stefan.competition;
+
+/**
+ * @description: 2609. 最长平衡子字符串
+ * @author: stefanyang
+ * @date: 2023/4/4 15:45
+ * @version: 1.0
+ */
+@SuppressWarnings("unused")
+public class Solution2609 {
+    public int findTheLongestBalancedSubstring(String s) {
+        int res = 0;
+        int len = s.length();
+        for (int i = 0; i < len; i++) {
+            int left = i, right = i + 1;
+            while (left >= 0 && right < len) {
+                if (s.charAt(left) != '0' || s.charAt(right) != '1') {
+                    break;
+                }
+                left--;
+                right++;
+            }
+            res = Math.max(res, right - left - 1);
+        }
+        return res;
+    }
+}
